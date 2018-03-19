3245e053f745b7751ec0d6f53c5c0c27ad98c1f5
travis <travislbrundage@gmail.com>
2018-03-19 11:43:20 -0700

    Proxy the legend graphic

--- src/common/legend/LegendDirective.js
+++ src/common/legend/LegendDirective.js
@@ -4,7 +4,7 @@
   var legendOpen = false;
 
   module.directive('loomLegend',
-      function($rootScope, mapService, serverService) {
+      function($rootScope, mapService, serverService, configService) {
         return {
           restrict: 'C',
           replace: true,
@@ -114,7 +114,23 @@
               } else {
                 domain = server.url;
               }
-
+              // Proxy the domain if appropriate
+              var getUseProxyParam = function(server) {
+                return goog.isDefAndNotNull(server.use_proxy) ? server.use_proxy : false;
+              };
+              // proxy a url if the souce parameters indicate to do so
+              var useProxyUrlParam = function(use_proxy, url) {
+                if (goog.isDefAndNotNull(use_proxy) && use_proxy === true) {
+                  url = decodeURIComponent(url);
+                  if (url.indexOf(configService.configuration.proxy) < 0) {
+                    url = configService.configuration.proxy + encodeURIComponent(url);
+                  } else {
+                    url = encodeURIComponent(url);
+                  }
+                }
+                return url;
+              };
+              domain = useProxyUrlParam(getUseProxyParam(server), domain);
               var params = {
                 request: 'GetLegendGraphic',
                 format: 'image/png',
